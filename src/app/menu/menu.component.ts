bf74d9444284ee1df7638b18572866b0b04c20cd
Henry León <haleont@gmail.com>
2020-02-24 15:53:40 -0500

    Promises part 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -16,7 +16,8 @@ export class MenuComponent implements OnInit {
   constructor(private dishService: DishService) { }
 
   ngOnInit(): void {
-    this.dishes = this.dishService.gerDishes();
+    this.dishService.gerDishes()
+    .then(dishes => this.dishes = dishes);
   }
 
   onSelect(dish: Dish){
